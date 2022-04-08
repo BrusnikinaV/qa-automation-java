be9dee19adb6f77d36a61592cee2f64f1e409894
v.brusnikina <v.brusnikina@tinkoff.ru>
2022-04-08 15:50:46 +0300

    Вывод сообщения, разделённого на страницы

--- src/main/java/com/tcs/edu/decorator/TimestampMessageDecorator.java
+++ src/main/java/com/tcs/edu/decorator/TimestampMessageDecorator.java
@@ -9,13 +9,17 @@ import java.time.Instant;
 public class TimestampMessageDecorator {
     static Integer messageCount = 0;
     /**
-     * Формирование строки вида  - текущая дата и время + строка
+     * Формирование строки вида  - номер строки текущая дата и время + строка
      * @param message Сообщение, которое необходимо добавить к текущей дате и времени
-     * @return Склейка текущий даты со временем и строки, полученной извне
+     * @return Сформированная строка по  шаблону
      */
     public static String decorate(String message){
+        Integer pageSize = 2;
         messageCount++;
-        //final var decoratedMessage =  messageCount + " " + Instant.now() + " " + message;
-        return String.format("%d %s %s", messageCount, Instant.now(), message);
+        if (messageCount % pageSize == 0){
+            return String.format("%d %s %s \n%s", messageCount, Instant.now(), message, "---");
+        }else{
+            return String.format("%d %s %s", messageCount, Instant.now(), message);
+        }
     }
 }
