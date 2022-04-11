22cc74dbecffcf7c2f56e99d31c01a7383445d50
v.brusnikina <v.brusnikina@tinkoff.ru>
2022-04-11 09:35:51 +0300

    PAGE_SIZE перенесена в глобальную переменную

--- src/main/java/com/tcs/edu/decorator/TimestampMessageDecorator.java
+++ src/main/java/com/tcs/edu/decorator/TimestampMessageDecorator.java
@@ -8,15 +8,15 @@ import java.time.Instant;
  */
 public class TimestampMessageDecorator {
     static Integer messageCount = 0;
+    static Integer PAGE_SIZE = 2;
     /**
      * Формирование строки вида  - номер строки текущая дата и время + строка
      * @param message Сообщение, которое необходимо добавить к текущей дате и времени
      * @return Сформированная строка по  шаблону
      */
     public static String decorate(String message){
-        Integer pageSize = 2;
         messageCount++;
-        if (messageCount % pageSize == 0){
+        if (messageCount % PAGE_SIZE == 0){
             return String.format("%d %s %s \n%s", messageCount, Instant.now(), message, "---");
         }else{
             return String.format("%d %s %s", messageCount, Instant.now(), message);
