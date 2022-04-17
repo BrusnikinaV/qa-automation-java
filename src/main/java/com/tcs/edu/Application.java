c42e3b54aff638c8d61f26aa061c63083c2f6255
v.brusnikina <v.brusnikina@tinkoff.ru>
2022-04-17 11:43:59 +0300

    добавлен enum Severity.java и метод преобразующий enum в string. В метод формирования стоки для печати добавлен параметр, отвечающий за значимость

--- src/main/java/com/tcs/edu/Application.java
+++ src/main/java/com/tcs/edu/Application.java
@@ -1,12 +1,16 @@
 package com.tcs.edu;
 
-import com.tcs.edu.decorator.TimestampMessageDecorator;
+import com.tcs.edu.decorator.MessageService;
+import com.tcs.edu.decorator.enumDecorator.Severity;
 
 class Application {
     public static void main(String[] args) {
-        System.out.println(TimestampMessageDecorator.decorate("Hello world"));
-        System.out.println(TimestampMessageDecorator.decorate("Hello world"));
-        System.out.println(TimestampMessageDecorator.decorate("Hello world"));
-        System.out.println(TimestampMessageDecorator.decorate("Hello world"));
+        System.out.println(MessageService.decorate("Hello world", Severity.MINOR));
+        System.out.println(MessageService.decorate("Hello world", Severity.REGULAR));
+        System.out.println(MessageService.decorate("Hello world", Severity.MAJOR));
+        System.out.println(MessageService.decorate("Hello world", Severity.MINOR));
+        System.out.println(MessageService.decorate("Hello world", Severity.REGULAR));
+        System.out.println(MessageService.decorate("Hello world", Severity.MAJOR));
+
     }
 }
---
+++ src/main/java/com/tcs/edu/decorator/enumDecorator/Severity.java
@@ -0,0 +1,9 @@
+package com.tcs.edu.decorator.enumDecorator;
+
+/**
+ * Enum, пречисляющий уровень важности
+ * @author v.brusnikina
+ */
+public enum Severity {
+    MINOR, REGULAR, MAJOR;
+}
