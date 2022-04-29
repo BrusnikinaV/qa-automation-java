019f6ae8b6164bcd3fb0eb21bca6c6a41e843663
v.brusnikina <v.brusnikina@tinkoff.ru>
2022-04-29 11:03:38 +0300

    Задание 2. Добавлен в API параметр, определяющий порядок вывода сообщений для последовательности строковых параметров vararg

--- src/main/java/com/tcs/edu/Application.java
+++ src/main/java/com/tcs/edu/Application.java
@@ -1,12 +1,13 @@
 package com.tcs.edu;
 
 import com.tcs.edu.decorator.MessageService;
+import com.tcs.edu.enums.MessageOrder;
 import com.tcs.edu.enums.Severity;
 
 class Application {
     public static void main(String[] args) {
         MessageService.print(Severity.MINOR, "Hello world!");
-        MessageService.print(Severity.REGULAR, "Hi!", "Hello world", "Hello", null);
-        MessageService.print(Severity.MAJOR, "Hello");
+        MessageService.print(Severity.REGULAR, MessageOrder.ASC, "Hi!", "Hello world", "Hello", null);
+        MessageService.print(Severity.MAJOR, MessageOrder.DESC, "Hello");
     }
 }
--- src/main/java/com/tcs/edu/decorator/MessageService.java
+++ src/main/java/com/tcs/edu/decorator/MessageService.java
@@ -1,5 +1,6 @@
 package com.tcs.edu.decorator;
 
+import com.tcs.edu.enums.MessageOrder;
 import com.tcs.edu.enums.Severity;
 import com.tcs.edu.printer.ConsolePrinter;
 
@@ -14,12 +15,26 @@ public class MessageService {
      * @param message Строка, которую необходимо вывести на консоль
      * @param messages Массив строк, каждую из которых необходимо вывести на консоль
      */
+
     public static void print(Severity level, String message, String... messages){
-        ConsolePrinter.print(MessageDecorate.decorate(message, level));
-        for (String currentMessage:messages){
-            if (currentMessage != null){
-                ConsolePrinter.print(MessageDecorate.decorate(currentMessage, level));
+        print(level, MessageOrder.ASC, message, messages);
+    }
+
+    public static void print(Severity level, MessageOrder order, String message, String... messages){
+        if (order.equals(MessageOrder.ASC)){
+            ConsolePrinter.print(MessageDecorate.decorate(message, level));
+            for (String currentMessage:messages){
+                if (currentMessage != null & level != null & message != null){
+                    ConsolePrinter.print(MessageDecorate.decorate(currentMessage, level));
+                }
+            }
+        } else {
+            for (int i = messages.length -1; i>=0; i--) {
+                if (messages[i] != null & level != null){
+                    ConsolePrinter.print(MessageDecorate.decorate(messages[i], level));
+                }
             }
         }
+
     }
 }
