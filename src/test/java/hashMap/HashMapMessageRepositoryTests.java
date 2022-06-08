7593ca989c35cc52b69147d34b1ff6d86b587fd0
v.brusnikina <v.brusnikina@tinkoff.ru>
2022-06-08 17:28:08 +0300

    Исправила замечания по ревью

--- src/test/java/hashMap/HashMapMessageRepositoryTests.java
+++ src/test/java/hashMap/HashMapMessageRepositoryTests.java
@@ -18,73 +18,79 @@ public class HashMapMessageRepositoryTests {
     private static UUID id1;
     private static UUID id2;
     private static UUID id3;
+
     @BeforeAll
-    static void createHashMap(){
+    static void createHashMap() {
         id1 = mapMessageRepository.addMessage(new Message(Severity.MAJOR, "A"));
         id2 = mapMessageRepository.addMessage(new Message(Severity.MINOR, "B"));
         id3 = mapMessageRepository.addMessage(new Message(Severity.MINOR, "B"));
+        //Проверим, что сгенерированы разные id
         assertNotEquals(id1, id2, "Сгенерированы одинаковые id");
         assertNotEquals(id1, id3, "Сгенерированы одинаковые id");
         assertNotEquals(id2, id3, "Сгенерированы одинаковые id");
     }
 
     @Nested
-    class FindAllMessages{
+    class FindAllMessages {
         @Test
-        void checkFindAllElementsInHashMap(){
+        void checkFindAllElementsInHashMap() {
             var returnCollection = mapMessageRepository.findAll();
             assertEquals(3, returnCollection.size(),
                     "Не совпало ожидаемое количество сообщений в результате поиска всех элементов");
-            var mes1 = returnCollection.stream().filter(hm->hm.getId().equals(id1)).findFirst().orElse(null);
-            var mes2 = returnCollection.stream().filter(hm->hm.getId().equals(id2)).findFirst().orElse(null);
-            var mes3 = returnCollection.stream().filter(hm->hm.getId().equals(id3)).findFirst().orElse(null);
-            assertNotNull(mes1, "Не нашли message по id");
-            assertNotNull(mes2, "Не нашли message по id");
-            assertNotNull(mes3, "Не нашли message по id");
-            assertEquals(Severity.MAJOR, mes1.getLevel(), "Не совпал Level");
-            assertEquals(Severity.MINOR, mes2.getLevel(), "Не совпал Level");
-            assertEquals(Severity.MINOR, mes3.getLevel(), "Не совпал Level");
-            assertEquals("A", mes1.getBody(), "Не совпал body");
-            assertEquals("B", mes2.getBody(), "Не совпал body");
-            assertEquals("B", mes3.getBody(), "Не совпал body");
+
+            var mes1 = returnCollection.stream().filter(hm -> hm.getId().equals(id1)).findFirst().orElse(null);
+            var mes2 = returnCollection.stream().filter(hm -> hm.getId().equals(id2)).findFirst().orElse(null);
+            var mes3 = returnCollection.stream().filter(hm -> hm.getId().equals(id3)).findFirst().orElse(null);
+
+            assertNotNull(mes1, "Не нашли message с id - " + mes1.getId());
+            assertNotNull(mes2, "Не нашли message с id - " + mes2.getId());
+            assertNotNull(mes3, "Не нашли message с id - " + mes3.getId());
+            assertEquals(Severity.MAJOR, mes1.getLevel(), "Не совпал Level у message c id - " + mes1.getId());
+            assertEquals(Severity.MINOR, mes2.getLevel(), "Не совпал Level у message c id - " + mes2.getId());
+            assertEquals(Severity.MINOR, mes3.getLevel(), "Не совпал Level у message c id - " + mes2.getId());
+            assertEquals("A", mes1.getBody(), "Не совпал body у message c id - " + mes1.getId());
+            assertEquals("B", mes2.getBody(), "Не совпал body у message c id - " + mes2.getId());
+            assertEquals("B", mes3.getBody(), "Не совпал body у message c id - " + mes3.getId());
         }
     }
 
     @Nested
-    class FindMessageByKey{
+    class FindMessageByKey {
         @Test
-        void checkFindElementsByKey(){
+        void checkFindElementsByKey() {
             var currentMessage = mapMessageRepository.findByPrimaryKey(id2);
-            assertEquals(Severity.MINOR, currentMessage.getLevel(), "Не совпал level по результатам поиска");
-            assertEquals("B", currentMessage.getBody(), "Не совпал body по результатам поиска");
+            assertEquals(Severity.MINOR, currentMessage.getLevel(), "Не совпал level по результатам поиска. id - " + id2);
+            assertEquals("B", currentMessage.getBody(), "Не совпал body по результатам поиска. id - " + id2);
         }
 
         @Test
-        void checkFindElementsByKeyWhereKeyEmptyInHashMap(){
+        void checkFindElementsByNotExistingKey() {
             var currentMessage = mapMessageRepository.findByPrimaryKey(UUID.randomUUID());
             assertNull(currentMessage, "В результате поиска по несуществующему id получили сообщение");
         }
     }
 
     @Nested
-    class FindMeassagesBySeverity{
+    class FindMeassagesBySeverity {
         @Test
-        void checkFindBySeverity(){
+        void checkFindBySeverity() {
             var returnCollection = mapMessageRepository.findBySeverity(Severity.MINOR);
             assertEquals(2, returnCollection.size(),
                     "Не совпало ожидаемое количество сообщений в результате поиска всех элементов");
-            var mes1 = returnCollection.stream().filter(hm->hm.getId().equals(id2)).findFirst().orElse(null);
-            var mes2 = returnCollection.stream().filter(hm->hm.getId().equals(id3)).findFirst().orElse(null);
-            assertNotNull(mes1, "Не нашли message по id");
-            assertNotNull(mes2, "Не нашли message по id");
-            assertEquals(Severity.MINOR, mes1.getLevel(), "Не совпал Level");
-            assertEquals(Severity.MINOR, mes2.getLevel(), "Не совпал Level");
-            assertEquals("B", mes1.getBody(), "Не совпал body");
-            assertEquals("B", mes2.getBody(), "Не совпал body");
+
+            var mes1 = returnCollection.stream().filter(hm -> hm.getId().equals(id2)).findFirst().orElse(null);
+            var mes2 = returnCollection.stream().filter(hm -> hm.getId().equals(id3)).findFirst().orElse(null);
+
+            assertNotNull(mes1, "Не нашли message по id. Level message - " + mes1.getLevel());
+            assertNotNull(mes2, "Не нашли message по id. Level message - " + mes1.getLevel());
+            assertEquals(Severity.MINOR, mes1.getLevel(), "Не совпал Level у message c id - " + mes1.getId());
+            assertEquals(Severity.MINOR, mes2.getLevel(), "Не совпал Level у message c id - " + mes2.getId());
+            assertEquals("B", mes1.getBody(), "Не совпал body у message c id - " + mes1.getId());
+            assertEquals("B", mes2.getBody(), "Не совпал body у message c id - " + mes2.getId());
         }
 
         @Test
-        void checkFindBySeverityWhereSeverityEmpty(){
+        void checkFindBySeverityWhereSeverityEmpty() {
             var currentMessage = mapMessageRepository.findBySeverity(Severity.REGULAR);
             assertEquals(0, currentMessage.size(), "В результате поиска по несуществующему id получили сообщение");
         }
